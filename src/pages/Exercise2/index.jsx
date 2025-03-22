bca333e5a8e94b2d1a21e062211c07080f2a3715
minhdang0 <dangnhatminh8402@gmail.com>
2025-03-22 18:04:10 +0700

    push

--- src/pages/Exercise2/index.jsx
+++ src/pages/Exercise2/index.jsx
@@ -2,11 +2,17 @@ import useLocalStorage from "../../hooks/useLocalStorage";
 
 function Exercise2() {
   const [name, setName] = useLocalStorage("name", "F8 User");
+  const [age, setAge] = useLocalStorage("age", 22);
+  const [address, setAddress] = useLocalStorage("address", "Ha Nam");
 
   return (
     <div>
       <input value={name} onChange={(e) => setName(e.target.value)} />
       <p>Xin chào, {name}!</p>
+      <input value={age} onChange={(e) => setAge(e.target.value)} />
+      <p>Minh {age} tuoi</p>
+      <input value={address} onChange={(e) => setAddress(e.target.value)} />
+      <p>Minh den tu {address}</p>
     </div>
   );
 }
